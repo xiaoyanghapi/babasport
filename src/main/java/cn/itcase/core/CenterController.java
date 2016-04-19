f93ea2b5d9ef61128afc4014208876425befefa7
xiaoyanghapi <xyyangjingjiang@163.com>
2016-04-19 13:49:19 +0800

    配置完成

---
+++ src/main/java/cn/itcase/core/CenterController.java
@@ -0,0 +1,15 @@
+package cn.itcase.core;
+
+import org.springframework.stereotype.Controller;
+
+/** 
+ * 后台管理
+ * 测试
+ * @author 作者 yjj: 
+ * @version 创建时间：2016年4月19日 上午11:10:59 
+ * 类说明 
+ */
+@Controller
+public class CenterController {
+
+}
