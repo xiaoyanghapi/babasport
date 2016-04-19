27d43150b77ac67c1fb60bad405bcc504e57e8ae
xiaoyanghapi <xyyangjingjiang@163.com>
2016-04-19 16:02:01 +0800

    ss

--- src/main/java/cn/itcast/core/service/TestTbServiceImpl.java
+++ src/main/java/cn/itcast/core/service/TestTbServiceImpl.java
@@ -1,8 +1,10 @@
 package cn.itcast.core.service;
 
 import javax.annotation.Resource;
+import javax.management.RuntimeErrorException;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import cn.itcast.core.bean.TestTable;
 import cn.itcast.core.dao.TestTbDao;
@@ -13,6 +15,7 @@ import cn.itcast.core.dao.TestTbDao;
  * 类说明 
  */
 @Service
+@Transactional
 public class TestTbServiceImpl implements TestTbService {
 	@Resource
 	private TestTbDao testTbDao;
@@ -20,6 +23,7 @@ public class TestTbServiceImpl implements TestTbService {
 	@Override
 	public void addTestTb(TestTable testTable) {
 		testTbDao.addTestTb(testTable);
+//		throw new RuntimeException();
 	}
 
 }
--- src/test/java/cn/itcast/TestTestTb.java
+++ src/test/java/cn/itcast/TestTestTb.java
@@ -3,11 +3,9 @@ package cn.itcast;
 import java.util.Date;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import cn.itcast.comment.junit.SpringJunitTest;
 import cn.itcast.core.bean.TestTable;
 import cn.itcast.core.service.TestTbService;
 
@@ -18,15 +16,13 @@ import cn.itcast.core.service.TestTbService;
  * @version 创建时间：2016年4月19日 下午2:26:14 
  * @ContextConfiguration(locations = "classpath:application-context.xml,后边可以跟很多")
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:application-context.xml")
-public class TestTestTb {
+public class TestTestTb extends SpringJunitTest{
 	@Autowired
 	private TestTbService testTbService;
 	@Test
 	public void testAdd() throws Exception {
 		TestTable testTable = new TestTable();
-		testTable.setName("杨景江");
+		testTable.setName("aaaa");
 		testTable.setBirthday(new Date());
 		testTbService.addTestTb(testTable);
 	}
