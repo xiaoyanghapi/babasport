a8eec91abf760009b74560b704d73cd446a1f1bf
xiaoyanghapi <xyyangjingjiang@163.com>
2016-04-19 16:42:05 +0800

    配置springmvc

---
+++ src/main/java/cn/itcast/comment/junit/SpringJunitTest.java
@@ -0,0 +1,20 @@
+package cn.itcast.comment.junit;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/** 
+ * 
+ * 测试公共类
+ * @author 作者 yjj: 
+ * @version 创建时间：2016年4月19日 下午3:58:45 
+ * 
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:application-context.xml")
+public class SpringJunitTest {
+
+	
+	
+}
