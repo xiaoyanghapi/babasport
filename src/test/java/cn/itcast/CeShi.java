6463c930fab650f6df4fb9bfd01e3319fa13b52c
Hapi <Hapi@Xiaoyanghapi>
2016-04-21 10:27:44 +0800

    哈哈

---
+++ src/test/java/cn/itcast/CeShi.java
@@ -0,0 +1,13 @@
+package cn.itcast;
+
+/** 
+ * @author 作者 yjj: 
+ * @version 创建时间：2016-4-21 上午10:24:37 
+ * 类说明 
+ */
+
+public class CeShi {
+	public static void main(String[] args) {
+		System.out.println("测试");
+	}
+}
